743dccbf79ea352f05e9bf9ccbe49e6dc4ae48ea
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-10-17 16:57:08 +0800

    fix(API): Routes updated to match the docs

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,23 @@ Route::post('login', [AuthAPIController::class, 'login']);
 
 // Resourceful API Routes (harder to apply authenticated access to)
 // Route::apiResource('authors', AuthorAPIController::class);
+//
 // We split the routes up into publicly accessible
 // and those requiring Authentication
 
 // Public API Routes
 Route::get("/authors", [AuthorAPIController::class, 'index']);
-Route::get("/authors/{id}", [AuthorAPIController::class, 'show']);
 
-// Authentication required API Routes
-// We wrap these in "Auth" Middleware
-//Route::group(['middleware' => ['auth:sanctum']], function () {
+
+// Authentication required API Routes (Auth-Sanctum Middleware)
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
     /* Prefix the given routes with /authors
      * Based on http://laravel-school.com/posts/building-a-delighted-restful-api-with-laravel-16
      */
     Route::prefix('authors')->group(function () {
 
-//        Route::get("{id}", [AuthorAPIController::class, 'show']);
+        Route::get("{id}", [AuthorAPIController::class, 'show']);
         Route::post('/', [AuthorAPIController::class, 'store']);
         Route::put('/{id}', [AuthorAPIController::class, 'update']);
         Route::delete('/{id}', [AuthorAPIController::class, 'destroy']);
@@ -49,7 +49,7 @@ Route::get("/authors/{id}", [AuthorAPIController::class, 'show']);
 
     /* Logout a logged-in user */
     Route::post('/logout', [AuthorAPIController::class, 'logout']);
-//});
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -73,9 +73,11 @@ Route::get('health', HealthCheckJsonResultsController::class);
  *
  * This is disabled, as a dummy and non-operational
  *
- * @response 400 scenario="Service is unhealthy" {"status": "down", "services": {"database": "up", "redis": "down"}}
- * @responseField status The status of this API (`up` or `down`).
- * @responseField services Map of each downstream service and their status (`up` or `down`).
+ * @response        400         scenario="Service is unhealthy"
+ *                              {"status": "down",
+ *                               "services": {"database": "up", "redis": "down"}}
+ * @responseField   status      The status of this API (`up` or `down`).
+ * @responseField   services    Map of each downstream service and their status (`up` or `down`).
  */
 Route::get('/healthcheck', function () {
     return response()->json([
@@ -91,5 +93,4 @@ Route::get('/healthcheck', function () {
 });
 
 /* Fallback route with nice error message */
-
-Route::fallback([ ApiFallbackController::class,'error']);
+Route::fallback([ApiFallbackController::class, 'error']);
