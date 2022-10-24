1a5ecb2a41768fa66b64fc9e2ca3bd06daebfa81
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-10-24 16:38:58 +0800

    fix(author): Author seeder - removed duplicating unknowns

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Author;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AuthorSeeder extends Seeder
@@ -16,32 +15,14 @@ class AuthorSeeder extends Seeder
     public function run()
     {
 
-        $unknownAuthors = [
-            [
-                'id' => 1,
-                'given_name' => 'UNKNOWN',
-                'family_name' => 'AUTHOR',
-                'is_company' => False,
-            ],
-            [
-                'id' => 2 ,
-                'family_name' => 'UNKNOWN CORPORATE AUTHOR',
-                'is_company' => True,
-            ],
-        ];
-
-        foreach ($unknownAuthors as $seedAuthor) {
-            Author::create($seedAuthor);
-        }
-
         $seedAuthors = [
-            [ 'given_name' => 'UNKNOWN', 'family_name' => 'AUTHOR',           'is_company' => False, ],
-            [ 'given_name' => 'UNKNOWN', 'family_name' => 'CORPORATE AUTHOR', 'is_company' => True,  ],
-            [ 'given_name' => 'Kevin',   'family_name' => 'Yank',             'is_company' => False, ],
-            [ 'given_name' => 'Mark',    'family_name' => 'Boulton',          'is_company' => False, ],
-            [ 'given_name' => 'Robert',  'family_name' => 'Hoekman, jr.',     'is_company' => False, ],
-            [ 'given_name' => 'Luke',    'family_name' => 'Wroblewski',       'is_company' => False, ],
-            [ 'given_name' => 'Kevin',   'family_name' => 'Potts',            'is_company' => False, ],
+            ['id' => 1, 'given_name' => 'UNKNOWN', 'family_name' => 'AUTHOR', 'is_company' => False,],
+            ['id' => 2, 'given_name' => 'UNKNOWN', 'family_name' => 'CORPORATE AUTHOR', 'is_company' => True,],
+            ['given_name' => 'Kevin', 'family_name' => 'Yank', 'is_company' => False,],
+            ['given_name' => 'Mark', 'family_name' => 'Boulton', 'is_company' => False,],
+            ['given_name' => 'Robert', 'family_name' => 'Hoekman, jr.', 'is_company' => False,],
+            ['given_name' => 'Luke', 'family_name' => 'Wroblewski', 'is_company' => False,],
+            ['given_name' => 'Kevin', 'family_name' => 'Potts', 'is_company' => False,],
         ];
 
         foreach ($seedAuthors as $seedAuthor) {
